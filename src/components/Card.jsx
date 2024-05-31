991a6657da753e8d95e97133fa9913bb691e240d
Nura <ainur.saduova10@gmail.com>
2024-05-31 13:32:47 -0400

    implemented carousel

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,6 @@
 function Card ({text, img, credentials, className}) {
     return (
         <div className={className}>
-            <svg id="curve" xmlns="http://www.w3.org/2000/svg" width="610" height="154"><path fill="#F4F4FC" fillRule="evenodd" d="M610 154C469.493 123.42 358.432 86.087 276.818 42S102.93-11.42 0 14v140h610z"/></svg>
             <div className="text-field">
                 <svg id="quotes" xmlns="http://www.w3.org/2000/svg" width="120" height="100"><defs><linearGradient id="a" x1="31.368%" x2="100%" y1="72.917%" y2="19.557%"><stop offset="0%" stopColor="#9975D0"/><stop offset="100%" stopColor="#4A3FDB"/></linearGradient></defs><path fill="url(#a)" fillRule="evenodd" d="M52.615 0L24.858 100H0V0h52.615zM120 0L92.243 100H67.385V0H120z" opacity=".15"/></svg>
                 <p>{text}</p>
